9f3ac3524c792c024f0323b1b7bb2b946d6670b0
Federico Roux <rouxfederico@gmail.com>
2023-05-17 11:01:54 -0300

    nrf52840dk Board Support on overlay dt (#8)

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,8 +2,16 @@
 
 BOARD=nucleo_u575zi_q
 
+if [ $# -eq 0 ]; then
+  echo "No board received. Using $BOARD as default"
+else
+  BOARD=$1
+  echo "Board: Using $BOARD"
+fi
+
+
 echo "Creating git version information files..."
 ./update_git_hash_on_file.sh
 ./update_git_branch_on_file.sh
 cd ..
-west build -b $BOARD .
+west build -b $BOARD . --pristine
