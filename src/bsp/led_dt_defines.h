414c79a3a5b49df62f46cf3a3e84429f027f6f27
fgr-17 <rouxfederico@gmail.com>
2023-05-17 13:59:24 +0000

    added nrf52840dk support on overlay dt

--- src/bsp/led_dt_defines.h
+++ src/bsp/led_dt_defines.h
@@ -11,9 +11,14 @@
 #define __LED_DT_DEFINES
 
 /* Get node directly without aliases */
-#define LED1_NODE DT_NODELABEL(green_led_1)
-#define LED2_NODE DT_NODELABEL(blue_led_1)
-#define LED3_NODE DT_NODELABEL(red_led_1)
+// #define LED1_NODE DT_NODELABEL(green_led_1)
+// #define LED2_NODE DT_NODELABEL(blue_led_0)
+// #define LED3_NODE DT_NODELABEL(red_led_1)
+
+#define LED1_NODE DT_ALIAS(l0)
+#define LED2_NODE DT_ALIAS(l1)
+#define LED3_NODE DT_ALIAS(l2)
+
 
 
 #endif // __LED_DT_DEFINES
